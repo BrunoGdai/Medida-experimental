a353c6fc1859635637464ac641c48291155bf64c
Bruno Gastaldi <brunodgastaldi@gmail.com>
2018-09-11 23:14:40 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Medida-experimental
---
+++ valorexp.py
@@ -0,0 +1,52 @@
+class ValorExp:
+    def __init__(self, val, inc):
+        self.val = val # valor medido
+        self.inc = inc # incerteza
+    def __neg__(self):
+        return ValorExp(-self.val, self.inc)
+        
+    def __add__(self, other):
+        if isinstance(other, ValorExp):
+            return ValorExp(self.val + other.val, self.inc + other.inc)
+        else:
+            return ValorExp(self.val + other, self.inc)
+    
+    __radd__ = __add__
+        
+    def __sub__(self, other):
+        if isinstance(other, ValorExp):
+            return ValorExp(self.val - other.val, self.inc + other.inc)
+        else:
+            return ValorExp(self.val - other, self.inc)
+    def __rsub__(self, other):
+        return -self.__sub__(other)
+    
+    def __mul__(self, other):
+        if isinstance(other, ValorExp):
+            return ValorExp(self.val*other.val, self.inc*other.val + self.val*other.inc)
+        else:
+            return ValorExp(self.val*other, self.inc*other)
+        
+    __rmul__ = __mul__
+
+    def __pow__(self, n): #n é real
+        return ValorExp(self.val**n, n*self.val**(n-1)*self.inc)
+    
+    def __truediv__(self, other):     
+        if isinstance(other, ValorExp):
+            if other.val == 0:
+                raise Exception("Divisão por zero")
+            else:
+                return ValorExp(self.val/other.val, (self.inc*other.val + self.val*other.inc)/other.val**2)
+        else:
+            if other == 0:
+                raise Exception("Divisão por zero")
+            else:
+                return ValorExp(self.val/other, self.inc/other)
+            
+    def __rtruediv__(self, other):
+        return other.__mul__(self.__pow__(-1))
+            
+    #__rtruediv__ = __mul__(__pow__(self, -1))
+    def __repr__(self):
+        return str(self.val) + ' +/- ' + str(self.inc)
